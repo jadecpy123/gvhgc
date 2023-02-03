98ebe8bd9ce8c77206e3267a9780bfe05dfa7720
Mithun <76549104+Agent-150@users.noreply.github.com>
2023-02-03 16:40:03 +0530

    Update render_template.py

--- Jade/utils/render_template.py
+++ Jade/utils/render_template.py
@@ -17,17 +17,17 @@ async def render_page(id, secure_hash):
         raise InvalidHash
     src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
     if str(file_data.mime_type.split('/')[0].strip()) == 'video':
-        async with aiofiles.open('Adarsh/template/req.html') as r:
+        async with aiofiles.open('Jade/template/req.html') as r:
             heading = 'Watch {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
-        async with aiofiles.open('Adarsh/template/req.html') as r:
+        async with aiofiles.open('Jade/template/req.html') as r:
             heading = 'Listen {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
     else:
-        async with aiofiles.open('Adarsh/template/dl.html') as r:
+        async with aiofiles.open('Jade/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
                 async with s.get(src) as u:
                     heading = 'Download {}'.format(file_data.file_name)
